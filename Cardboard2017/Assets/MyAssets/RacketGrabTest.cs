62a92f49b75361f8e5011074bd2c268132249586
ginja_gabriel@hotmail.com <lightning6c90d88a>
2018-08-28 13:53:32 -0300

    28/08/2018 13:52 raquete com movimentação corrigida

--- Cardboard2017/Assets/MyAssets/RacketGrabTest.cs
+++ Cardboard2017/Assets/MyAssets/RacketGrabTest.cs
@@ -13,6 +13,12 @@ public class RacketGrabTest : MonoBehaviour {
     Material m_Material;
     public Material normal, grabbed;
     public Vector3 initialpos;
+    public GameObject BAT;
+    //Vectors & Quaternion
+    public Quaternion direction;
+    public Vector3 Leapdirection;
+    public Vector3 LeapPosition;
+
 
     void Start () {
         controller = new Controller();
@@ -28,20 +34,42 @@ public class RacketGrabTest : MonoBehaviour {
             hand = frame.Hands[0];
 
         }
-        IsRacketGrabbed();
+        
+        
+            IsRacketGrabbed();
+            RotateRacket();
+            TranslateRacket();
+        
+        
 	}
+    private void RotateRacket()
+    {
+        Leapdirection = ToVector3(new Vector(-hand.PalmNormal.x, -hand.PalmNormal.z, hand.PalmNormal.y));
+        transform.rotation = Quaternion.LookRotation(Leapdirection);
+    }
+
     private void IsRacketGrabbed()
     {
         if (hand.GrabStrength == 1)
         {
             Debug.Log("grabbed");
             m_Material.color = grabbed.color;
-            transform.position = initialpos + new Vector3(hand.PalmPosition.x / 500, hand.PalmPosition.y / 500, hand.PalmPosition.z / 500);
+            //transform.position = initialpos + new Vector3(hand.PalmPosition.x / 500, hand.PalmPosition.y / 500, hand.PalmPosition.z / 500);
         }
         else
         {
             m_Material.color = normal.color;
-            transform.position = initialpos;
+           // transform.position = initialpos;
         }
+    }
+
+    private void TranslateRacket()
+    {
+        LeapPosition = ToVector3(new Vector(-hand.PalmPosition.x, -hand.PalmPosition.z, 0)/100);
+        transform.position =BAT.transform.position + LeapPosition;
+    }
+    public static Vector3 ToVector3(Vector v)
+    {
+        return new Vector3(v.x, v.y, v.z);
     }
 }
